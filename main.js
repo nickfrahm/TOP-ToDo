01181d81138415521afe88065e840df171b4825d
nickfrahm <nfrahm24@gmail.com>
2021-06-01 17:38:36 -0500

    add to do form

--- main.js
+++ main.js
@@ -16,7 +16,27 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_UI__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/UI */ \"./src/modules/UI.js\");\n\n\n_modules_UI__WEBPACK_IMPORTED_MODULE_0__.UI.loadPage();\n\n//# sourceURL=webpack://TOP-ToDo/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_UI__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/UI */ \"./src/modules/UI.js\");\n\n\n_modules_UI__WEBPACK_IMPORTED_MODULE_0__.UI.addEvents();\n\n//# sourceURL=webpack://TOP-ToDo/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/modules/Mask.js":
+/*!*****************************!*\
+  !*** ./src/modules/Mask.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Mask\": () => (/* binding */ Mask)\n/* harmony export */ });\nconst Mask = () => {\n    const Mask = document.createElement(\"div\");\n    \n    if (document.getElementById(\"mask\")) {\n        document.getElementById(\"mask\").classList.toggle(\"hide\");\n    } else {\n        const Mask = document.createElement(\"div\");\n        Mask.classList.add(\"mask\");\n        Mask.id = \"mask\";\n        Mask.addEventListener(\"click\", removeMask);\n\n        return Mask;\n    }\n}\n\nconst removeMask = () => {\n    const Mask = document.querySelector(\".mask\");\n    Mask.classList.toggle(\"hide\");\n}\n\n//# sourceURL=webpack://TOP-ToDo/./src/modules/Mask.js?");
+
+/***/ }),
+
+/***/ "./src/modules/Modal.js":
+/*!******************************!*\
+  !*** ./src/modules/Modal.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"AddModal\": () => (/* binding */ AddModal)\n/* harmony export */ });\nconst AddModal = (type) => {\n    const AddModal = document.createElement(\"div\");\n    AddModal.classList.add(\"modal\");\n\n    const modalHeader = document.createElement(\"h2\");\n    modalHeader.classList.add(\"modalHeader\");\n    modalHeader.textContent = `Add ${type}`;\n    AddModal.appendChild(modalHeader);\n\n    const modalForm = document.createElement(\"form\");\n    AddModal.appendChild(modalForm);\n\n    if (type === \"ToDo\") {\n        //title\n        const titleLabel = document.createElement(\"label\");\n        titleLabel.textContent = \"Title\";\n        titleLabel.htmlFor = \"title\";\n        titleLabel.classList.add(\"label\");\n        const titleInput = document.createElement(\"input\");\n        titleInput.placeholder = \"I need to...\";\n        titleInput.classList.add(\"input\");\n        titleInput.id = \"title\";\n        modalForm.appendChild(titleLabel);\n        modalForm.appendChild(titleInput);\n\n        //description\n        const descriptionLabel = document.createElement(\"label\");\n        descriptionLabel.textContent = \"Description\";\n        descriptionLabel.htmlFor = \"description\";\n        descriptionLabel.classList.add(\"label\");\n        const descriptionInput = document.createElement(\"textarea\");\n        descriptionInput.placeholder = \"Doing thing after doing other things...\";\n        descriptionInput.classList.add(\"textArea\");\n        descriptionInput.id = \"description\";\n        modalForm.appendChild(descriptionLabel);\n        modalForm.appendChild(descriptionInput);\n\n        //due date\n        const dueDateLabel = document.createElement(\"label\");\n        dueDateLabel.textContent = \"Due Date\";\n        dueDateLabel.htmlFor = \"dueDate\";\n        dueDateLabel.classList.add(\"label\");\n        const dueDateInput = document.createElement(\"input\");\n        dueDateInput.type = \"date\"\n        dueDateInput.placeholder = \"MM/DD/YY\";\n        dueDateInput.classList.add(\"input\");\n        dueDateInput.id = \"dueDate\";\n        modalForm.appendChild(dueDateLabel);\n        modalForm.appendChild(dueDateInput);\n\n        //priority\n        const priorityLabel = document.createElement(\"label\");\n        priorityLabel.textContent = \"Priority\";\n        priorityLabel.htmlFor = \"priority\";\n        priorityLabel.classList.add(\"label\");\n        modalForm.appendChild(priorityLabel);\n\n        const prioritySelect = document.createElement(\"select\");\n        prioritySelect.id = \"priority\";\n        prioritySelect.name = \"priority\";\n        modalForm.appendChild(prioritySelect);\n\n        const priorityOptions = [\"High\", \"Medium\", \"Low\"];\n        for (let i = 0; i < priorityOptions.length; i++) {\n            const option = document.createElement(\"option\");\n            option.value = priorityOptions[i];\n            prioritySelect.appendChild(option);\n        }\n\n        //add button\n        const addButton = document.createElement(\"button\");\n        addButton.classList.add(\"btn addTodo\");\n        addButton.value = \"Add\"\n        \n    }\n\n    return AddModal;\n}\n\n//# sourceURL=webpack://TOP-ToDo/./src/modules/Modal.js?");
 
 /***/ }),
 
@@ -30,13 +50,23 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 
 /***/ }),
 
+/***/ "./src/modules/ProjectList.js":
+/*!************************************!*\
+  !*** ./src/modules/ProjectList.js ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"ProjectList\": () => (/* binding */ ProjectList)\n/* harmony export */ });\nclass ProjectList {\n\n}\n\n//# sourceURL=webpack://TOP-ToDo/./src/modules/ProjectList.js?");
+
+/***/ }),
+
 /***/ "./src/modules/Storage.js":
 /*!********************************!*\
   !*** ./src/modules/Storage.js ***!
   \********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Storage\": () => (/* binding */ Storage)\n/* harmony export */ });\nclass Storage {\n  //get projects from storage\n  static getProjectsFromStorage() {\n    return [\n      {\n        \"name\": \"test 1\",\n        \"tasks\": [\n            \n        ],\n      },\n      {\n        \"name\": \"test 2\",\n        \"tasks\": [],\n      },\n      \n    ];\n    //return JSON.parse(localStorage.getItem(\"projects\"));\n  }\n}\n\n\n//# sourceURL=webpack://TOP-ToDo/./src/modules/Storage.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Storage\": () => (/* binding */ Storage)\n/* harmony export */ });\nclass Storage {\n  //get projects from storage\n  static getProjectsFromStorage() {\n    \n  }\n}\n\n\n//# sourceURL=webpack://TOP-ToDo/./src/modules/Storage.js?");
 
 /***/ }),
 
@@ -56,7 +86,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \***************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"UI\": () => (/* binding */ UI)\n/* harmony export */ });\n/* harmony import */ var _ToDo__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ToDo */ \"./src/modules/ToDo.js\");\n/* harmony import */ var _Project__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Project */ \"./src/modules/Project.js\");\n/* harmony import */ var _Storage__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Storage */ \"./src/modules/Storage.js\");\n\n\n\n\nclass UI {\n    static loadPage() {\n        UI.addEvListeners();\n        const projects = _Storage__WEBPACK_IMPORTED_MODULE_2__.Storage.getProjectsFromStorage();\n        console.log(projects);\n        UI.writeProjectsToScreen(projects);\n        UI.writeToDosToScreen();\n    }\n\n    static writeProjectsToScreen(projects) {\n        projects.forEach(project => {\n            const p = document.createElement(\"p\");\n            console.log(project.name)\n            p.textContent = project.name;\n            document.querySelector(\".projects__header\").after(p);\n        })\n    }\n\n    static addEvListeners() {\n        document.querySelector(\".addProject\").addEventListener(\"click\", () => {\n            _Project__WEBPACK_IMPORTED_MODULE_1__.Project.addProject();\n            UI.addProjectToList();\n        });\n\n    }\n    \n}\n\n//# sourceURL=webpack://TOP-ToDo/./src/modules/UI.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"UI\": () => (/* binding */ UI)\n/* harmony export */ });\n/* harmony import */ var _ToDo__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ToDo */ \"./src/modules/ToDo.js\");\n/* harmony import */ var _Project__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Project */ \"./src/modules/Project.js\");\n/* harmony import */ var _Storage__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Storage */ \"./src/modules/Storage.js\");\n/* harmony import */ var _ProjectList__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./ProjectList */ \"./src/modules/ProjectList.js\");\n/* harmony import */ var _Mask__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./Mask */ \"./src/modules/Mask.js\");\n/* harmony import */ var _Modal__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./Modal */ \"./src/modules/Modal.js\");\n\n\n\n\n\n\n\nclass UI {\n    static addTodoForm() {\n        document.querySelector(\".main\").appendChild((0,_Mask__WEBPACK_IMPORTED_MODULE_4__.Mask)());\n        document.getElementById(\"mask\").appendChild((0,_Modal__WEBPACK_IMPORTED_MODULE_5__.Modal)());\n        console.log(\"addTodoForm\")\n    }\n    \n    static addEvents() {\n        document.querySelector(\".addTodo\").addEventListener(\"click\", UI.addTodoForm);\n    }\n}\n\n//# sourceURL=webpack://TOP-ToDo/./src/modules/UI.js?");
 
 /***/ })
 
